2c4b29b85a8ad0559a4331cfc42275be0019991a
Aryan-Thapa <aryanthapa219@gmail.com>
2024-11-09 18:30:55 +0530

    useNavigate added

--- Lecture-10/React-Router/src/components/Dashboard.jsx
+++ Lecture-10/React-Router/src/components/Dashboard.jsx
@@ -1,14 +1,24 @@
 // import React from 'react'
 
-import { Outlet } from "react-router-dom"
-
+import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const handleCourses = () => {
+    navigate("courses");
+  };
+
+  const handleReports = () => {
+    navigate("reports");
+  };
   return (
     <div>
       <h1>DashBoard</h1>
-      <Outlet/>
+      <button onClick={handleCourses}>Courses</button>
+      <button onClick={handleReports}>Reports</button>
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
